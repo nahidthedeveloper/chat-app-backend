c8015d583de28017d24b23e41e27837f81091ecf
nahid <nahidcse.pub@gmail.com>
2024-05-15 17:03:50 +0600

    add custom admin and channel authentication

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,34 +1,20 @@
-# import os
-#
-# from channels.auth import AuthMiddlewareStack
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from chat.routers import websocket_urlpatterns
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-# application = get_asgi_application()
-#
-# application = ProtocolTypeRouter({
-#     "http": application,
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(websocket_urlpatterns)
-#     )
-# })
-
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat.routers import websocket_urlpatterns
+from chat.middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
+        TokenAuthMiddleware(
             URLRouter(websocket_urlpatterns)
         )
     ),
 })
+
+
+
--- chat/consumers.py
+++ chat/consumers.py
@@ -8,28 +8,32 @@ class PersonalChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs'].get('room_name')
         self.room_group_name = 'chat_%s' % self.room_name
+        user = self.scope['user']
 
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
+        if user and user.is_authenticated:
+            self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
 
-        self.accept()
-        self.send(text_data=json.dumps({
-            'connect_message': 'Socket connected',
-        }))
+            self.accept()
+            self.send(text_data=json.dumps({
+                'connect_message': 'Socket connected',
+            }))
+        else:
+            self.close(code=4401)
 
-    def chat_message(self, event):
-        message = event['message']
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
 
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
+def chat_message(self, event):
+    message = event['message']
+    self.send(text_data=json.dumps({
+        'message': message
+    }))
 
+
+def disconnect(self, close_code):
+    # Leave room group
+    async_to_sync(self.channel_layer.group_discard)(
+        self.room_group_name,
+        self.channel_name
+    )
